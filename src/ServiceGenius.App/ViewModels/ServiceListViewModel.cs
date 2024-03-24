1c046fa1004b07b7d47ca0734a607b1f16c4a065
Luandersonn Airton <luandersonn@outlook.com>
2024-03-24 10:43:35 -0300

    Refactor: Non-blocking service load method for UI responsiveness

--- src/ServiceGenius.App/ViewModels/ServiceListViewModel.cs
+++ src/ServiceGenius.App/ViewModels/ServiceListViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.ServiceProcess;
+using System.Threading.Tasks;
 
 namespace ServiceGenius.App.ViewModels;
 
@@ -12,19 +14,33 @@ public class ServiceListViewModel : ObservableObject
     {
         Services = [];
 
-        RefreshServicesCommand = new RelayCommand(RefreshServices);
+        RefreshServicesCommand = new AsyncRelayCommand(RefreshServicesAsync);
     }
 
     public ObservableCollection<ServiceControllerViewModel> Services { get; }
-    public IRelayCommand RefreshServicesCommand { get; }
+    public IAsyncRelayCommand RefreshServicesCommand { get; }
 
-    private void RefreshServices()
+    private async Task RefreshServicesAsync()
     {
-        ServiceController[] services = ServiceController.GetServices();
-        Services.Clear();
-        foreach (ServiceController service in services.OrderBy(s => s.DisplayName))
+        Task task = Task.Delay(500);
+
+        List<ServiceControllerViewModel> viewModels = await Task.Run(() =>
         {
-            Services.Add(new ServiceControllerViewModel(service));
+
+            ServiceController[] services = ServiceController.GetServices();
+
+            return services.OrderBy(s => s.ServiceName)
+                           .Select(service => new ServiceControllerViewModel(service))
+                           .ToList();
+        });
+
+        Services.Clear();
+
+        foreach (ServiceControllerViewModel viewModel in viewModels)
+        {
+            Services.Add(viewModel);
         }
+
+        await task;
     }
 }
