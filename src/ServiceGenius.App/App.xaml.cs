897bac88ce22b0e2c3de8645dd6ce2132900c2a8
Luandersonn Airton <luandersonn@outlook.com>
2024-03-29 08:59:52 -0300

    Configure app as single instance

--- src/ServiceGenius.App/App.xaml.cs
+++ src/ServiceGenius.App/App.xaml.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.UI.Xaml;
 using ServiceGenius.App.Pages;
 using ServiceGenius.App.Windowing;
+using System;
 using System.Collections.Concurrent;
 using System.Diagnostics;
+using WinRT;
+using WinUIEx;
+using MUX = Microsoft.UI.Xaml;
+using MWA = Microsoft.Windows.AppLifecycle;
+using WPA = Windows.ApplicationModel.Activation;
 
 namespace ServiceGenius.App;
 
@@ -15,7 +21,7 @@ public partial class App : Application
         UnhandledException += OnAppUnhandledException;
     }
 
-    private void OnAppUnhandledException(object sender, UnhandledExceptionEventArgs e)
+    private void OnAppUnhandledException(object sender, MUX.UnhandledExceptionEventArgs e)
     {
         e.Handled = true;
         Debug.WriteLine(e.Message);
@@ -24,12 +30,39 @@ public partial class App : Application
     public static ConcurrentDictionary<XamlRoot, Window> AllWindows { get; } = [];
     public static GeniusWindow MainWindow { get; private set; }
 
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
     {
+        MWA.AppActivationArguments appArgs = MWA.AppInstance.GetCurrent().GetActivatedEventArgs();
+
+        MWA.AppInstance mainInstance = MWA.AppInstance.FindOrRegisterForKey("main");
+
+        if (!mainInstance.IsCurrent)
+        {
+            await mainInstance.RedirectActivationToAsync(appArgs);
+
+            Process.GetCurrentProcess().Kill();
+            return;
+        }
+
         MainWindow = new GeniusWindow(typeof(ServiceListViewPage))
         {
             PersistenceId = "MainWindow"
         };
         MainWindow.Activate();
+
+        MWA.AppInstance.GetCurrent().Activated += OnAppActivated;
+    }
+
+
+
+    private void OnAppActivated(object sender, MWA.AppActivationArguments e) => HandleActivation(e.Data.As<WPA.IActivatedEventArgs>());
+
+    private static void HandleActivation(WPA.IActivatedEventArgs _)
+    {
+        MainWindow?.DispatcherQueue.TryEnqueue(() =>
+            {
+                IntPtr hwnd = MainWindow.GetWindowHandle();
+                HwndExtensions.RestoreWindow(hwnd);
+            });
     }
 }
