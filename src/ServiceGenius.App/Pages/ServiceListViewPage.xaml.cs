fff81ece7a6956a89caa2a64d2037c16963aff05
Luandersonn Airton <luandersonn@outlook.com>
2024-03-29 21:42:05 -0300

    Improve search

--- src/ServiceGenius.App/Pages/ServiceListViewPage.xaml.cs
+++ src/ServiceGenius.App/Pages/ServiceListViewPage.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using ServiceGenius.App.ViewModels;
 using System;
+using System.Threading.Tasks;
 
 namespace ServiceGenius.App.Pages;
 
@@ -11,31 +12,37 @@ public sealed partial class ServiceListViewPage : Page
 
     protected override void OnNavigatedTo(NavigationEventArgs e) => ViewModel.RefreshServicesCommand.Execute(null);
 
-    private async void OnListViewItemClick(object sender, ItemClickEventArgs e)
+    private async void OnListViewItemClick(object sender, ItemClickEventArgs e) => await ShowServiceDetails((ServiceControllerViewModel)e.ClickedItem);
+
+    private void GoToSettingsPage() => Frame.Navigate(typeof(SettingsPage));
+
+    private async void OnSearchBoxTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
     {
-        _ = await new Controls.ServiceDetailsDialog((ServiceControllerViewModel)e.ClickedItem)
+        StringComparison sc = StringComparison.InvariantCultureIgnoreCase;
+
+        string query = sender.Text;
+
+        if (string.IsNullOrWhiteSpace(query))
+        {
+            ViewModel.Services.Filter = null;
+        }
+        else
+        {
+            await Task.Delay(250);
+
+            if (args.CheckCurrent())
+            {
+                ViewModel.Services.Filter = item => item.ServiceName.Contains(query, sc) || item.DisplayName.Contains(query, sc) || item.Description.Contains(query, sc);
+            }
+        }
+    }
+
+    private async Task ShowServiceDetails(ServiceControllerViewModel service)
+    {
+        _ = await new Controls.ServiceDetailsDialog(service)
         {
             XamlRoot = XamlRoot,
             RequestedTheme = ActualTheme
         }.ShowAsync();
-    }
-
-    private void GoToSettingsPage() => Frame.Navigate(typeof(SettingsPage));
-
-    private void OnSearchBoxTextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
-    {
-        if (args.CheckCurrent())
-        {
-            string queryTerm = sender.Text;
-            if (string.IsNullOrWhiteSpace(queryTerm))
-            {
-                ViewModel.Services.Filter = null;
-            }
-            else
-            {
-                StringComparison sc = StringComparison.InvariantCultureIgnoreCase;
-                ViewModel.Services.Filter = item => item.ServiceName.Contains(queryTerm, sc) || item.DisplayName.Contains(queryTerm, sc) || item.Description.Contains(queryTerm, sc);
-            }
-        }
     }
 }
